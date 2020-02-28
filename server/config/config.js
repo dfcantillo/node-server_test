cb3e942cec464dec556e63d193d433d46bf7362a
dfcantillo <dfcantillo@emergiacc.com>
2020-02-27 23:38:11 -0500

    Variables de enterno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'develop';
 // Base de datos
 // =================================================
 let urlDB = (process.env.NODE_ENV  === 'develop') ? 'mongodb://localhost:27017/claveDelMarDB' :
-'mongodb+srv://admin:ucZy3gyXKmqKQkRd@cluster0-oy3h9.mongodb.net/claveDelMarDB';
-// urlDB = 'mongodb+srv://admin:ucZy3gyXKmqKQkRd@cluster0-oy3h9.mongodb.net/claveDelMarDB';
+process.env.MONGO_URL;
 
 process.env.URLDB = urlDB;
